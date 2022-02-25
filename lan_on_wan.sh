f4061fca0912a1b6b26cd5ec27a2186dc86583b8
zhudan <464033315@qq.com>
2022-02-26 00:20:02 +0800

    Update lan_on_wan.sh

--- lan_on_wan.sh
+++ lan_on_wan.sh
@@ -3,6 +3,6 @@
 # 此程序用于用untagged的vlan通过wan延伸lan.wan走tagged vlan(华硕原厂支持,lan-IPTV,在lan端口设置互联网VID),lan走untagged vlan
 #
 
-ifconfig eth0 down
 brctl addif br0 eth0
-ifconfig eth0 up
+
+logger "lan已通过br0桥接eth0"
