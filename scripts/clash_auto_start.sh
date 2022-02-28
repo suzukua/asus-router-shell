be70d4e56666629d34f596d0afdd8c56e089fe83
zhudan <464033315@qq.com>
2022-02-28 09:41:51 +0800

    Create clash_auto_start.sh

---
+++ scripts/clash_auto_start.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#
+# shellClash自启动脚本
+#
+
+#shellClash安装目录
+clashdir=/tmp/mnt/sda1/.asus/clash
+
+profile=/etc/profile
+
+sed -i '/alias clash=*/'d $profile
+echo "alias clash=\"$shtype $clashdir/clash.sh\"" >> $profile
+sed -i '/export clashdir=*/'d $profile
+echo "export clashdir=\"$clashdir\"" >> $profile
+. /etc/profile
+#sleep 10
+. $clashdir/start.sh start
