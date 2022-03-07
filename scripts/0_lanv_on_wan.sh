d4698210f25504d731581eb751bc33fc141c8497
zhudan <464033315@qq.com>
2022-03-07 22:22:18 +0800

    Update 0_lanv_on_wan.sh

--- scripts/0_lanv_on_wan.sh
+++ scripts/0_lanv_on_wan.sh
@@ -6,8 +6,15 @@
 #LAN延伸到外部的VLAN ID
 LAN_VLAN_ID=10
 
-vconfig add "eth0" "$LAN_VLAN_ID"
-brctl addif "br0" "vlan$LAN_VLAN_ID"
-ifconfig "vlan$LAN_VLAN_ID" up
+
+# vconfig add "eth0" "$LAN_VLAN_ID"
+# brctl addif "br0" "vlan$LAN_VLAN_ID"
+# ifconfig "vlan$LAN_VLAN_ID" up
+
+
+ip link add link eth0 name eth0.$LAN_VLAN_ID type vlan id $LAN_VLAN_ID
+ip link set eth0.$LAN_VLAN_ID up
+brctl addif br0 eth0.$LAN_VLAN_ID
+
 logger "lan通过br0桥接eth0成功"
 
