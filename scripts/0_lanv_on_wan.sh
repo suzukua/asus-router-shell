ea2e5d839011a78f95cfc7d4e77922af594cdece
zhudan <464033315@qq.com>
2022-03-07 18:44:39 +0800

    Create 0_lanv_on_wan.sh

---
+++ scripts/0_lanv_on_wan.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# 单线复用wan口, 通过vlan的方式延伸lan
+#
+
+#LAN延伸到外部的VLAN ID
+LAN_VLAN_ID=10
+
+if [[ -z "$( brctl show | grep -o "eth0.$LAN_VLAN_ID" | sed -n 1p )" ]];
+then
+  vconfig add "eth0" "$LAN_VLAN_ID"
+  brctl addif "br0" "eth0"."$LAN_VLAN_ID"
+  ifconfig "eth0"."$LAN_VLAN_ID" up
+  logger "lan通过br0桥接eth0成功"
+else
+  logger "eth0已桥接至br0,跳过执行本脚本"
+fi
+
