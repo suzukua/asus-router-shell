e74e378ee1e337740cd139e232652042b4b62456
zhudan <464033315@qq.com>
2022-03-08 00:32:16 +0800

    Update 1_clash_auto_start.sh

--- scripts/1_clash_auto_start.sh
+++ scripts/1_clash_auto_start.sh
@@ -10,7 +10,7 @@ clashdir=/tmp/mnt/sda1/.asus/clash
 profile=/etc/profile
 
 sed -i '/alias clash=*/'d $profile
-echo "alias clash=\"$shtype $clashdir/clash.sh\"" >> $profile
+echo "alias clash=\"$clashdir/clash.sh\"" >> $profile
 sed -i '/export clashdir=*/'d $profile
 echo "export clashdir=\"$clashdir\"" >> $profile
 . /etc/profile
