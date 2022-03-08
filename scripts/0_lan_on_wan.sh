3c6a738934868726d371cc5c53bc73304a275472
zhudan <464033315@qq.com>
2022-03-08 09:36:03 +0800

    Update 0_lan_on_wan.sh

--- scripts/0_lan_on_wan.sh
+++ scripts/0_lan_on_wan.sh
@@ -7,7 +7,7 @@
 if [[ -z "$( brctl show | grep -o eth0 | sed -n 1p )" ]];
 then
   brctl addif br0 eth0
-  logger "lan通过br0桥接eth0成功"
+  logger -st "($(basename $0))" $$ "lan通过br0桥接eth0成功"
 else
-  logger "eth0已桥接至br0,跳过执行本脚本"
+  logger -st "($(basename $0))" $$ "eth0已桥接至br0,跳过执行本脚本"
 fi
