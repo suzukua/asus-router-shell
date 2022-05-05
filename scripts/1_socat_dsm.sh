cdc6107153cb588299d4d4acda73cba7abb226d6
zhudan <464033315@qq.com>
2022-05-05 12:28:56 +0800

    Update and rename 1_socat_5000.sh to 1_socat_dsm.sh

---
+++ scripts/1_socat_dsm.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# 更新间隔 分钟
+SOCAT_INTERVAL=99
+
+start_socat_5001() {
+        if [ -z "$(ps | grep socat | grep TCP6-LISTEN:5001)" ]; then
+                nohup socat TCP6-LISTEN:5001,reuseaddr,fork TCP4:192.168.100.4:5001 >/dev/null 2>&1 &
+                logger -st "($(basename $0))" $$ "启动成功：socat TCP6-LISTEN:5001,reuseaddr,fork TCP4:192.168.100.4:5001"
+        fi
+        if [ -z "$(ip6tables -L -n |grep 5001)" ]; then
+                ip6tables -I INPUT -p tcp --dport 5001 -j ACCEPT
+                logger -st "($(basename $0))" $$ "ip6tables添加成功：ip6tables -I INPUT -p tcp --dport 5001 -j ACCEPT"
+        fi
+}
+
+
+# write_cron_job(){
+    #每x分钟检查一次
+    #cru a socat_5001  "*/$SOCAT_INTERVAL * * * * $(readlink -f "$0")"
+# }
+
+kill_cron_job() {
+        if [ -n "$(cru l | grep socat_5001)" ]; then
+                logger "删除socat_5001定时更新任务..."
+                cru d socat_5001
+                #sed -i '/f3322/d' /var/spool/cron/crontabs/* >/dev/null 2>&1
+        fi
+}
+
+case $action in
+start)
+        logger -st "($(basename $0))" $$ "[socat_5001]: 开始执行socat_5001脚本"
+        start_socat_5001
+        ;;
+*)
+        start_socat_5001
+        ;;
