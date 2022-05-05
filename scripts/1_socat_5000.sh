ff53ba9d25bfadc138d4e18e065a98701c37bdc9
zhudan <464033315@qq.com>
2022-05-05 11:32:11 +0800

    Update 1_socat_5000.sh

--- scripts/1_socat_5000.sh
+++ scripts/1_socat_5000.sh
@@ -8,7 +8,6 @@ SOCAT_INTERVAL=99
 start_socat_5000(){
         if [ -z "$(ps | grep socat | grep TCP6-LISTEN:5000)" ]; then
             nohup socat TCP6-LISTEN:5000,reuseaddr,fork TCP4:192.168.100.4:5000 >/dev/null 2>&1 &
-            #write_cron_job
             logger -st "($(basename $0))" $$ "启动成功：socat TCP6-LISTEN:5000,reuseaddr,fork TCP4:192.168.100.4:5000"
         fi
         if [ -z "$(ip6tables -L -n |grep 5000)" ]; then
@@ -18,10 +17,10 @@ start_socat_5000(){
 }
 
 
-write_cron_job(){
+# write_cron_job(){
     #每x分钟检查一次
     #cru a socat_5000  "*/$SOCAT_INTERVAL * * * * $(readlink -f "$0")"
-}
+# }
 
 kill_cron_job() {
         if [ -n "$(cru l | grep socat_5000)" ]; then
