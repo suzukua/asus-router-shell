a8fcbd1750456175ce2de1b99049e26aab7e7b2a
zhudan <464033315@qq.com>
2023-02-20 09:52:24 +0800

    Update V03restartwireless.sh

--- scripts/V03restartwireless.sh
+++ scripts/V03restartwireless.sh
@@ -2,7 +2,8 @@
 # 开启后重启一次WiFi，解决AX86U部分物联网设备连接WiFi无法上网(无法获取IP)的问题
 # 重启UPNP，解决UPNP不稳定问题
 
-logger -st "($(basename $0))" $$ "开始执行重启WiFi"
+logger -st "($(basename $0))" $$ "开始执行重启WiFi，等待10s之后执行"
+sleep 10
 i=0
 while [ $i -le 20 ]; do
       success_start_service=`nvram get success_start_service`
