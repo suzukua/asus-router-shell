58c9d5407ad1f1cdb8e5de4b4f481d26bed7886e
suzuki <464033315@qq.com>
2024-08-11 12:48:00 +0800

    Update and rename V04iptv.sh to V03iptv.sh

--- scripts/V03iptv.sh
+++ scripts/V03iptv.sh
@@ -11,7 +11,7 @@ if ! ip link show "$INTERFACE" > /dev/null 2>&1; then
     # set MAC
     ip link set dev "$INTERFACE" address C4:FF:1F:59:B8:0E
 else
-    echo "Interface $INTERFACE already exists."
+    logger -st "($(basename $0))" $$ "Interface $INTERFACE already exists."
 fi
 
 # Check if the PID file exists and the process is running
@@ -20,9 +20,9 @@ PID_FILE="/var/run/udhcpc_iptv.pid"
 if [ -f "$PID_FILE" ]; then
     PID=$(cat "$PID_FILE")
     if [ -d "/proc/$PID" ]; then
-        echo "udhcpc is running with PID $PID. release ip..."
+        logger -st "($(basename $0))" $$ "udhcpc is running with PID $PID. release ip..."
         kill -SIGUSR2 `cat $PID_FILE `
-        echo "udhcpc is running with PID $PID. Stopping it..."
+        logger -st "($(basename $0))" $$ "udhcpc is running with PID $PID. Stopping it..."
         sleep 1
         kill "$PID"
         # Wait for the process to stop
@@ -30,12 +30,12 @@ if [ -f "$PID_FILE" ]; then
         # Remove the PID file
         rm -f "$PID_FILE"
     else
-        echo "PID file exists but no running process with PID $PID."
+        logger -st "($(basename $0))" $$ "PID file exists but no running process with PID $PID."
         rm -f "$PID_FILE"
     fi
 fi
 
 # Start udhcpc
 # 0x3d clientid
-udhcpc -b -i eth0.43 -p "$PID_FILE" -s /koolshare/init.d/iptv.script -x hostname:XXXXX -x 0x3d:XXX -V SCITV -A5 --syslog
+udhcpc -b --syslog -i eth0.43 -p "$PID_FILE" -s /koolshare/init.d/iptv.script -x hostname:XXXXX -x 0x3d:XXX -V SCITV -A5
 
